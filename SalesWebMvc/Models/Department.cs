c3666aaa4217924e9f0b5bb8b0e5e38040ee5b4d
Antonio Carlos <acdona@hotmail.com>
2020-11-18 14:10:03 -0300

    Segunda migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,8 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWebMvc.Models
 {
     public class Department
@@ -6,5 +10,32 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //construtores
+        public Department()
+        {
+        }
+
+        public Department(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public void RemoveSeller(Seller seller)
+        {
+            Sellers.Remove(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
